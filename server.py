22cbf41eeb9a869513282bc3842c5b2b67517ed7
JoubertCastro <jou.castrosousa@gmail.com>
2025-08-24 15:23:53 -0300

    ajuste timezone

--- server.py
+++ server.py
@@ -68,9 +68,9 @@ def init_db():
 
 def ajustar_timestamp(ts: str):
     try:
-        return datetime.fromtimestamp(int(ts), tz=timezone.utc) - timedelta(hours=3)
+        return datetime.fromtimestamp(int(ts), tz=timezone.utc) - timedelta(hours=6)
     except Exception:
-        return datetime.now(timezone.utc) - timedelta(hours=3)
+        return datetime.now(timezone.utc) - timedelta(hours=6)
 
 def salvar_mensagem(remetente, mensagem, msg_id=None, nome=None, timestamp=None, raw=None):
     data_hora = ajustar_timestamp(timestamp) if timestamp else datetime.now(timezone.utc) - timedelta(hours=3)
